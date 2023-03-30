724b826eb2853a5487049cf0727646ad1b36170a
Franklin Jara <franklinjaradev@gmail.com>
2023-03-29 21:41:37 -0400

    :sparkles: chore: deatils update

--- src/pages/workout/[workoutId].tsx
+++ src/pages/workout/[workoutId].tsx
@@ -2,18 +2,45 @@ import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
-import { api } from "src/utils/api";
 
+import Spinner from "src/components/common/spinner";
+import Error from "src/components/common/error";
+import { api } from "src/utils/api";
 import { ssgHelper } from "src/utils/ssg";
+import UserMenu from "src/components/common/user-menu";
+import Head from "next/head";
 
 const WorkoutId = ({
   workoutId,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { data, isError, isLoading } = api.workout.getWorkoutById.useQuery({
-    workoutId: workoutId as string,
-  });
+  const { data, isError, isLoading, error } =
+    api.workout.getWorkoutById.useQuery({
+      workoutId: workoutId as string,
+    });
 
-  return <div>WorkoutId</div>;
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <Error message={error.message} />;
+  }
+
+  return (
+    <UserMenu>
+      <Head>
+        <title>{data ? data.name : "Workout"}</title>
+      </Head>
+      {data && (
+        <div className="flex flex-col gap-1">
+          <h2 className="custom-h2 text-center">{data.name}</h2>
+          <p className="custom-p">Description: {data.description ?? ""}</p>
+          {data.notes && <p className="custom-p">Notes: {data.notes}</p>}
+          <p className="custom-p">Copy count: {data.copyCount}</p>
+        </div>
+      )}
+    </UserMenu>
+  );
 };
 
 export default WorkoutId;
