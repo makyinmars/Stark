8e99fe2ed1e17d167d878cddc00e572094761766
Franklin Jara <franklinjaradev@gmail.com>
2023-04-19 23:00:35 -0400

    :sparkles: feat: style update for upgrade page

--- src/pages/upgrade.tsx
+++ src/pages/upgrade.tsx
@@ -4,6 +4,13 @@ import { useRouter } from "next/router";
 
 import UserMenu from "src/components/common/user-menu";
 import { Button } from "src/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "src/components/ui/card";
 import { api } from "src/utils/api";
 import { ssgHelper } from "src/utils/ssg";
 
@@ -55,39 +62,92 @@ const Upgrade = () => {
       </Head>
       <UserMenu>
         {!isLoading && subscriptionStatus !== null && (
-          <div className="flex flex-col gap-4">
-            <h2 className="custom-h2 self-center">
-              Your subscription is{" "}
-              <span className="text-green-600">{subscriptionStatus}</span>
-            </h2>
-            <Button onClick={() => void onCreateBillingPortalSession()}>
-              Manage subscription and billing
-            </Button>
-          </div>
+          <Card className="flex flex-col gap-4">
+            <CardHeader>
+              <CardTitle>
+                <h2 className="custom-h2 self-center">
+                  Your subscription is{" "}
+                  <span className="text-green-600">{subscriptionStatus}</span>
+                </h2>
+              </CardTitle>
+            </CardHeader>
+            <CardFooter>
+              <Button
+                className="w-full"
+                onClick={() => void onCreateBillingPortalSession()}
+              >
+                Manage subscription and billing
+              </Button>
+            </CardFooter>
+          </Card>
         )}
-        {!isLoading && subscriptionStatus === null && (
-          <div className="flex flex-col gap-4">
-            <h2 className="custom-h2 self-center">Premiun Membership</h2>
-            <div className="flex justify-around">
-              <div className="flex flex-col justify-around rounded border-2 border-slate-800 p-4">
-                <h3 className="custom-h3 self-center">Free</h3>
-                <ul>
-                  <li>Create workouts</li>
-                </ul>
-              </div>
-              <div className="flex flex-col justify-around gap-4 rounded border-2 border-slate-800 p-4">
-                <h3 className="custom-h3 self-center">Premiun</h3>
-                <ul>
-                  <li>Free plan plus</li>
-                  <li>Copy workouts</li>
-                  <li>View chart</li>
-                </ul>
-                <Button onClick={() => void onCreateCheckoutSession()}>
-                  Upgrade account
-                </Button>
-              </div>
-            </div>
-          </div>
+        {!isLoading && !subscriptionStatus === null && (
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <h2 className="custom-h2">Premium Membership</h2>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-1 gap-4 md:grid-cols-2">
+              <Card>
+                <CardHeader>
+                  <CardTitle>
+                    <h3 className="custom-h3 self-center">Free</h3>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    <li>
+                      Access to workout plans and routines created by Stark
+                    </li>
+                    <li>Basic progress tracking features</li>
+                    <li>Limited access to community features</li>
+                    <li>
+                      No ability to copy workouts or view progress in chart
+                      format
+                    </li>
+                    <li>No premium support from Stark</li>
+                  </ul>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle>
+                    <h3 className="custom-h3 self-center">Premium</h3>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ul>
+                    <li>Access to all features of the free membership</li>
+                    <li>
+                      Ability to copy workouts created by other users or
+                      trainers on the app
+                    </li>
+                    <li>Save favorite workouts for easy access later</li>
+                    <li>
+                      View progress in visual and easy-to-understand chart
+                      format
+                    </li>
+                    <li>Set goals and benchmarks for yourself</li>
+                    <li>Track progress towards these goals over time</li>
+                    <li>Priority support from Stark</li>
+                    <li>
+                      More in-depth community features and support from other
+                      premium members
+                    </li>
+                  </ul>
+                </CardContent>
+                <CardFooter>
+                  <Button
+                    className="w-full"
+                    onClick={() => void onCreateCheckoutSession()}
+                  >
+                    Upgrade account
+                  </Button>
+                </CardFooter>
+              </Card>
+            </CardContent>
+          </Card>
         )}
       </UserMenu>
     </>
